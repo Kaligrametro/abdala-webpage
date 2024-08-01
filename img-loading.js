f477832ba9555a38af6e5204ef6c807dd664c061
Kaligrametro <kaligrametro@gmail.com>
2024-07-31 23:36:44 -0300

    Fixes

--- img-loading.js
+++ img-loading.js
@@ -1,14 +1,31 @@
+import fs from 'fs';
+fs.readdir('./fotos', (err, files) => {});
+console.log(files);
+
 for(let i = 1; i < 37; i++) {
   createImageLayout(i);
 }
 
+const fs = require('fs');
+
+/*
+for (let i = 1; true; i++)
+{
+  const images = require(`./fotos/img${i}.jpg`);
+  console.log(images);
+
+}*/
+
+
+
 function createImageLayout(number){
     let galeriaDeFotos = document.getElementsByClassName("galeriaFotos")[0];
+
     let a = document.createElement("a");
-    a.href = "fotos-tiny/img" + number + ".jpg";
-    a.style.backgroundImage = "url(fotos-tiny/img" + number + ".jpg)";
-    a.classList.add("layout-image");
-    a.classList.add("layout-image-" + number);
+    a.href = `fotos/img${number}.jpg`;
+    a.style.backgroundImage = `url(fotos/img${number}.jpg)`;
+    a.classList.add(`layout-image`);
+    a.classList.add(`layout-image-${number}`);
 
     let ion_icon = document.createElement("ion-icon");
     ion_icon.name = "expand-outline";
